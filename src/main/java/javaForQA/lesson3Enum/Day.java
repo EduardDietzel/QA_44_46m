c8b77d9174de8f093ac47c58f0bc1ecf4dc0f5cf
Eduard Dietzel <banderol.ed@gmail.com>
2023-03-15 19:36:59 +0100

    Лекция по нумерации от 10.03.2023
    Signed-off-by: Eduard Dietzel <banderol.ed@gmail.com>

---
+++ src/main/java/javaForQA/lesson3Enum/Day.java
@@ -0,0 +1,30 @@
+package javaForQA.lesson3Enum;
+
+public enum Day {
+
+    // все константы п умолчанию public static final
+    // помимо самих значений константы могут содержать еще какие-то значения
+    SUNDAY("Воскресенье"),
+    MONDAY("Понедельник"),
+    TUESDAY("Вторник"),
+    WEDNESDAY("Среда"),
+    THURSDAY("Четверг"),
+    FRIDAY("Пятница"),
+    SATURDAY("Суббота");
+
+    private String value;
+
+    private Day(String value) {
+        this.value = value;
+        // System.out.println("Отработал конструктор с параметром " + value);
+    }
+
+    // Джава-машина создает все перечисленные объекты
+    Day() {
+        // System.out.println("Отработал пустой конструктор Day");
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
