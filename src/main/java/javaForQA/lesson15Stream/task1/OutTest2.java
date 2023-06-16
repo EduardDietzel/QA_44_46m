38317dbce3f29d1fa92c60588332cf751169e680
Eduard Dietzel <banderol.ed@gmail.com>
2023-06-16 12:38:19 +0200

    Added a new files for new lesson15 at 16.06.2023
    Signed-off-by: Eduard Dietzel <banderol.ed@gmail.com>

---
+++ src/main/java/javaForQA/lesson15Stream/task1/OutTest2.java
@@ -0,0 +1,10 @@
+package javaForQA.lesson15Stream.task1;
+
+public class OutTest2 {
+    public static void main(String[] args) {
+
+        System.out.println("One! ");
+        System.out.println("Two! ");
+        System.out.println("Three! ");
+    }
+}
